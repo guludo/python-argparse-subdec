f2c98ab211323a3d8d2d28a3b0ca4bbd95218f30
Gustavo Sousa <gustavo.jo.sousa@gmail.com>
2021-11-23 16:41:06 -0300

    chore: release v0.1.0

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
+import pathlib
+
 import setuptools
 
 setuptools.setup(
     name='argparse-subdec',
-    version='0.0.0',
+    version='0.1.0',
+    long_description=(pathlib.Path(__file__).parent / 'README.rst').read_text(),
+    long_description_content_type='text/x-rst',
+    url='https://github.com/guludo/python-argparse-subdec',
     packages=['argparse_subdec'],
     extras_require={
         'tests': [
@@ -10,4 +15,10 @@ setuptools.setup(
             'coverage~=5.5',
         ],
     },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Programming Language :: Python :: 3',
+    ],
 )
